5b919a43af6ee5402ab1b3ba3849c1dd8c43904e
canercan <92014299+canercand@users.noreply.github.com>
2022-04-30 12:28:56 +0300

    Degisken bölümü ödevi
    www.patika.dev

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace degisken 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string? name = Console.ReadLine();
+        }
+    }
+}
